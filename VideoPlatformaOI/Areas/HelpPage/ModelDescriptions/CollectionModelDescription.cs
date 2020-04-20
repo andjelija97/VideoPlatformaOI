ee3d9766bb59cd50f2e66b9409a8524e2ef638c3
Andjelija <Mihajlovic>
2020-04-20 14:54:20 +0200

    Commit FE PT20

---
+++ VideoPlatformaOI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VideoPlatformaOI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
